4756a8dfab6ce8e955730cfbaa10d60b61441cb1
elcholak <elcholak@gmail.com>
2022-10-01 18:44:27 +0300

    gevent socketio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
 import datetime as dt
 import os
+import logging
 import flask as fl
 import flask_socketio as fl_sock
 import config
@@ -8,19 +8,18 @@ import config
 os.makedirs('./logs', exist_ok=True)
 logging.basicConfig(
     level=logging.DEBUG,
-    handlers=[logging.FileHandler(f'./logs/{dt.datetime.now().isoformat()}.log')]
+    handlers=[logging.FileHandler(f'logs/{dt.datetime.now().isoformat()}.log')]
 )
 
-async_mode='gevent'
+
+async_mode = 'gevent'
 app = fl.Flask(__name__)
 app.config['SECRET_KEY'] = config.SECRET_KEY
-socketio = fl_sock.SocketIO(appasync_mode=async_mode, path='socket.io',
-                            cors_allowed_origins=["http://127.0.0.1:8080",
-                                                  "http://localhost:4200",
-                                                  "http://localhost:4201",
-                                                  "https://gusev-vladislav.ru"],
-                            logging=logging.getLogger(__name__))
-
+# socketio = fl_sock.SocketIO(app)
+socketio = fl_sock.SocketIO(app, async_mode=async_mode, path='socket.io', cors_allowed_origins=["http://127.0.0.1:8080",
+                                                                                        "http://localhost:4200",
+                                                                                        "http://localhost:4201",
+                                                                                        "https://gusev.vladislav.ru"])
 
 @app.route('/')
 def index():
